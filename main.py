a3924f02b76e95362348cba062519b15f0b7bd30
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-11-19 18:16:13 +0430

    add conditions and make some upgrade the code 🎯📏

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 
-
-weight = int(input("Your Weight: "))
-height = int(input("Your Height: "))
-
 def calculate_bmi(weight, height):
     return weight / ((height / 100) ** 2)
 
+def main():
+    weight = int(input("Your Weight: "))
+    height = int(input("Your Height: "))
 
-print(calculate_bmi(weight, height))
+    bmi = calculate_bmi(weight, height)
+
+    if(bmi < 18.5):
+        print(f"Underweight, your bmi is {bmi}")
+
+    elif(bmi >= 18.5 and bmi <= 24.9):
+        print(f"Normal weight, your bmi is {bmi}")
+
+    elif(bmi >= 25 and bmi <= 29.9):
+        print(f"Overweight, your bmi is {bmi}")
+
+    elif(bmi >= 30):
+        print(f"Obesity, your bmi is {bmi}")
+    
+main()
 
 
