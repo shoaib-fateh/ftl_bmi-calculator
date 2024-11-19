79d74c2ee613d9999f8aa7bd548d4af2a11e7879
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-11-19 18:30:09 +0430

    add comments #️⃣

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 
+
+# here bmi calculation will done
 def calculate_bmi(weight, height):
     return weight / ((height / 100) ** 2)
 
+
+# the main function
 def main():
 
+    # this will get the system (metric or imperial)
     system = input("Your System: metric (m) or imperial (i): ")
 
+    # this if condition will check that which system it is
     if(system == "m"):
         weight = int(input("Your Weight in kilogram: "))
         height = int(input("Your Height in centimeters: "))
@@ -14,11 +20,15 @@ def main():
         height = int(input("Your Height in inches: ")) / 39.3701
     else:
         print("Try again.")
+
+        # re agin the function
         return main()
         
 
+    # here it will do calculation and then store it in bmi varible
     bmi = calculate_bmi(weight, height)
 
+    # here it will check the bmi and print it out
     if(bmi < 18.5):
         print(f"Underweight, your bmi is {bmi}")
 
@@ -31,6 +41,9 @@ def main():
     elif(bmi >= 30):
         print(f"Obesity, your bmi is {bmi}")
     
+
+
+# running the main loop to begin the mini app
 main()
 
 
